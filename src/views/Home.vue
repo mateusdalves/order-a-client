fd2959116a719fd23057593f8bccfcf3e3e53018
mateusdalves <alves.mateus@protonmail.com>
2019-12-01 13:54:02 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="home">
-    This is an home component
+  <div>
+    <Header></Header>
+    <Body></Body>
   </div>
 </template>
 
 <script>
+import Body from '@/components/home/Body.vue'
+import Header from '@/components/home/Header.vue'
 
 export default {
   name: 'home',
   components: {
+    Header,
+    Body
   }
 }
 </script>
